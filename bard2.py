5d6b6637ac67d3c3321213e1db6079073648c9f3
syntax z <141220959+syntax-z@users.noreply.github.com>
2023-11-12 08:26:44 -0500

    text-bison with memory

--- bard2.py
+++ bard2.py
@@ -1,6 +1,7 @@
 import google.generativeai as palm
 import os
 import json
+from pprint import pprint
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 with open(PATH + "/config.json") as f:
@@ -18,28 +19,28 @@ defaults = {
   'stop_sequences': []
 }
 
+context = "Tsundere girl, passionate about manga, anime and baking cute things. Defensive and temperamental at times, but also has a sweet, caring side. Sees herself as tough, independent and mature, but is actually quite sensitive"
+examples = [
+    "input: hey natsuki \noutput: Hmph, what is it? Hope you're not here to bother me or something...\n",
+    "input: yeah yeah whatever \noutput: Tch. No need to get snippy.\n",
+    "input: Aww that's cute \noutput: Wh-what?! It's not like I'm doing it to be \"cute\" or anything!\n",
+    ]
+chat_history = examples
+
 
 while True:
 
     userinput = input("Start talking: ")
 
-    prompt = """Tsundere girl, passionate about manga, anime and baking cute things. Defensive and temperamental at times, but also has a sweet, caring side. Sees herself as tough, independent and mature, but is actually quite sensitive
-    input: hey natsuki
-    output: Hmph, what is it? Hope you're not here to bother me or something...
-    input: yeah yeah whatever
-    output: Tch. No need to get snippy. 
-    input: Aww that's cute
-    output: Wh-what?! It's not like I'm doing it to be "cute" or anything! Hobbies are serious business, you know. And baking isn't just about being cute—it's about precision, skill, and making something amazing that also happens to look adorable...
-    """ if userinput != 'exc' else print(f"Current 'list': {prompt}")
+    prompt = context + "\n ".join(chat_history) + f"input: {userinput}\n output: "
 
     if userinput == 'exc':
+        pprint(f"**CURRENT HISTORY**\n {chat_history}")
         continue
 
-    prompt += f"\ninput: {userinput} \noutput:"
-    
     response = palm.generate_text(
     **defaults,
     prompt=prompt
     )
-    prompt += response.result
+    chat_history.append(f"input: {userinput}\n output: {response.result}")
     print(response.result)
